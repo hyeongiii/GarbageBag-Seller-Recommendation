c6be1f8f1a17e626424ed7f194858ef3c3ec0bf1
hyeongiii <hyeongiii0811@gmail.com>
2023-02-13 22:20:29 +0900

    :tada: init: 프로젝트 생성

---
+++ src/main/java/com/example/garbagebag/GarbagebagApplication.java
@@ -0,0 +1,13 @@
+package com.example.garbagebag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbagebagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbagebagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/garbagebag/GarbagebagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garbagebag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbagebagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
